25933adb899d48755905c663b0dfa26d18521651
sala-design <69428338+sala-design@users.noreply.github.com>
2020-10-16 11:01:35 +0100

    Add files via upload

---
+++ sala.js
@@ -0,0 +1,9 @@
+function ghf() {
+    let cfbh = document.getElementById("menu");
+    cfbh.classList.toggle("active");
+
+    document.getElementById("bar1").classList.toggle("active");
+    document.getElementById("bar2").classList.toggle("active");
+    document.getElementById("bar3").classList.toggle("active");
+}
+
